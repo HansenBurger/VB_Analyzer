11ea3ffc21dd890f8a090d6a3a44424ce94ad677
HansenBurger <1035376945@eqq.com>
2023-03-22 17:22:31 +0800

    analyzer design 1. complete main function 2. correlation analyzer 3. coherence analyzer

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import numpy as np
-from modules.analyzers import TimeMetrics, Correlations
+import pandas as pd
+import seaborn as sns
 from matplotlib import pyplot as plt
+from modules.analyzers import AnalyzerBase, TimeMetrics, Correlations, Coherence
 
 npy_path = r'src_data\simul_emp.npy'
 npy_data = np.load(npy_path)
 exp_0 = npy_data[0]
 
-# g_var = TimeMetrics.global_variance(exp_0)
-# print("Subject 1's Global Variance:{0}".format(round(g_var, 3)))
+variance = TimeMetrics.global_variance(exp_0)
+stability, synchrony = TimeMetrics.proxy_metastable_sync(exp_0)
+kuramoto = TimeMetrics.kuramoto_index(exp_0)
 
-# stable, sync = TimeMetrics.proxy_metastable_sync(exp_0)
-a = exp_0[0:20, :]
-# g_var = TimeMetrics.global_variance(a)
-# stable, sync = TimeMetrics.proxy_metastable_sync(a)
-# print("Subject 1's Global Variance:{0}".format(round(g_var, 3)))
-# TimeMetrics.kuramoto_index(a)
-# TimeMetrics.kuramoto_index(a, way='stft', nperseg=64, noverlap=32)
-Correlations.cross_correlation_coefficient(a[0], a[1])
-a = 1
+print("Subject 1:\n")
+print("\t Global Variance:\t{0}".format(round(variance, 3)))
+print("\t Proxy Metastability:\t{0}".format(round(stability, 3)))
+print("\t Proxy synchrony:\t{0}".format(round(synchrony, 3)))
+print("\t Kuramoto index:\t{0}".format(round(kuramoto, 3)))
+
+exp_0_partial = exp_0[0:25, :]
+
+a, b = Coherence.cross_coherence(exp_0_partial[0],
+                                 exp_0_partial[1],
+                                 nperseg=64)
+
+corr_base = AnalyzerBase.auto_matrix_genrator
+corr_func_0 = Correlations.cross_correlation_coefficient
+corr_func_1 = Correlations.temporal_covariance
+corr_fucn_2 = Correlations.pearson_correlation
+fig, (ax_0, ax_1, ax_2) = plt.subplots(1, 3, figsize=(16, 5))
+
+coefficient = np.max(corr_base(exp_0_partial, corr_func_0), axis=2)
+sns.heatmap(coefficient, ax=ax_0, cmap="coolwarm")
+ax_0.set_title("Cross-correlation(Max)")
+
+covariance = corr_base(exp_0_partial, corr_func_1)
+sns.heatmap(covariance, ax=ax_1, cmap="rocket_r")
+ax_1.set_title("Temporal covariance")
+
+pearson = corr_base(exp_0_partial, corr_fucn_2)
+sns.heatmap(pearson, ax=ax_2, cmap="coolwarm")
+ax_2.set_title("Pearson correlation")
+
+fig.tight_layout()
+plt.show()
+plt.close()
--- modules/analyzers.py
+++ modules/analyzers.py
@@ -1,28 +1,26 @@
 import numpy as np
 import pandas as pd
 from scipy.stats import pearsonr
-from scipy.signal import stft, hilbert, get_window, correlate, coherence, signaltools
+from scipy.signal import detrend, stft, hilbert, get_window, correlate, coherence, signaltools
 
 
 class AnalyzerBase():
     def __init__(self) -> None:
         pass
 
-    @classmethod
-    def assertions(cls, arr: np.ndarray, axis: int):
+    @staticmethod
+    def assertions(arr: np.ndarray, axis: int):
+        #TODO:
         pass
 
-    @classmethod
-    def zero_mean(cls, arr: np.ndarray, axis: int = 0):
-        assert arr.shape[axis] > 1
-        assert type(arr) == np.ndarray
-
+    @staticmethod
+    def zero_mean(arr: np.ndarray, axis: int = 0):
         arr_mean = arr.mean(axis=axis, keepdims=True)
         arr_zero_mean = arr - arr_mean
         return arr_zero_mean
 
     @classmethod
-    def hilbert_trans(cls, arr: np.ndarray, axis: int = 0):
+    def hilbert_trans(cls, arr: np.ndarray, axis: int = 0, **kwargs):
         arr_zero_mean = cls.zero_mean(arr, axis)
         arr_norm = arr_zero_mean / np.std(arr, axis=axis, keepdims=True)
         analytic_signal = hilbert(arr_norm)
@@ -45,8 +43,30 @@ class AnalyzerBase():
         avg_phases = np.mean(phases, axis=1)  # 平均所有窗口的相位信息
         return avg_phases
 
+    @staticmethod
+    def auto_matrix_genrator(arr: np.ndarray, func: any,
+                             axes: tuple = (0, 1)) -> np.ndarray:
+        n_rows, n_cols = arr.shape[axes[0]], arr.shape[axes[0]]
+        auto_grid = np.empty((n_rows, n_cols), dtype=object)
 
-class TimeMetrics(AnalyzerBase):
+        for i in range(n_rows):
+            for j in range(n_rows):
+                auto_grid[i, j] = func(arr[i], arr[j])
+
+        cell_shape = auto_grid[0, 0].shape
+
+        if len(cell_shape) == 1 and cell_shape[0] == 1:
+            corr_matrix = auto_grid
+        else:
+            corr_matrix = np.zeros((n_rows, n_cols, *cell_shape))
+            for i in range(n_rows):
+                for j in range(n_rows):
+                    corr_matrix[i, j] = auto_grid[i, j]
+
+        return corr_matrix
+
+
+class TimeMetrics():
     @staticmethod
     def global_variance(arr: np.ndarray, axes: tuple = (0, 1)) -> float:
         '''
@@ -65,8 +85,7 @@ class TimeMetrics(AnalyzerBase):
         return global_var
 
     @staticmethod
-    def proxy_metastable_sync(arr: np.ndarray, axes: tuple = (0, 1),
-                              **kwargs) -> tuple:
+    def proxy_metastable_sync(arr: np.ndarray, axes: tuple = (0, 1)) -> tuple:
         '''
         Calculate the spatial coherence of a group of nodes
 
@@ -107,40 +126,136 @@ class TimeMetrics(AnalyzerBase):
             'hilbert': AnalyzerBase.hilbert_trans,
             'stft': AnalyzerBase.stft_trans
         }
-        arr_phases = trans_method(arr, axes[1], **kwargs)
+        arr_phases = trans_method[way](arr, axes[1], **kwargs)
         r_s = np.abs(np.mean(np.exp(1j * np.array(arr_phases)), axis=axes[0]))
         r_final = np.mean(r_s)
         return r_final
 
 
-class Correlations(AnalyzerBase):
-    @classmethod
-    def cross_correlation_coefficient(cls, arr_0: np.ndarray,
-                                      arr_1: np.ndarray):
-        print(1)
+class Correlations():
+    @staticmethod
+    def cross_correlation_coefficient(arr_0: np.ndarray,
+                                      arr_1: np.ndarray,
+                                      mode: str = 'same') -> np.ndarray:
+        '''
+        Calculate two arrays' Cross-correlation coefficient
 
-    @classmethod
-    def atuo_correlation_matrix(arr: np.ndarray,
-                         mode: str = 'same',
-                         axes: tuple = (0, 1)) -> np.ndarray:
-        arr_zero_mean = AnalyzerBase.zero_mean(arr, axes[1])
-        auto_correlate = correlate(arr_zero_mean, arr_zero_mean, mode='same')
-        auto_coefficient = auto_correlate / np.sum(arr_zero_mean**2)
-        a = 1
-        pass
+        Args:
+            arr_0: input array 0 \n
+            arr_1: input array 1 \n
+            mode: mode setting used in `scipy.signal.correlate`, default: "same"
+
+        Return:
+            np.ndarray: cross-correlation coefficient
+        '''
+        std_0 = np.std(arr_0)
+        std_1 = np.std(arr_1)
+        arr_0_zero_mean = AnalyzerBase.zero_mean(arr_0)
+        arr_1_zero_mean = AnalyzerBase.zero_mean(arr_1)
+        correlation = correlate(arr_0_zero_mean, arr_1_zero_mean, mode=mode)
+
+        def normalize_factor(mode: str = mode):
+            n_0, n_1 = arr_0.shape[0], arr_1.shape[0]
+            N = {
+                'same': max(n_0, n_1),
+                'valid': max(n_0, n_1) - min(n_0, n_1) + 1,
+                'full': n_0 + n_1 - 1
+            }
+            normalized_factor = min(n_0, n_1) * np.ones(N[mode])
+            # TODO: norm_func for full mode
+            # normalized_factor = np.vectorize(norm_func)(norm_arr)
+            return normalized_factor
+
+        coefficient = correlation / (normalize_factor() * std_0 * std_1)
+        return coefficient
 
     @staticmethod
-    def temporal_covariance(
-        arr: np.ndarray, axes: tuple = (0, 1)) -> np.ndarray:
-        pass
+    def temporal_covariance(arr_0: np.ndarray, arr_1: np.ndarray) -> float:
+        '''
+        Calculate two arrays' Temporal covariance
+
+        Args:
+            arr_0: input array 0 \n
+            arr_1: input array 1
+        
+        Return:
+            float: temporal covariance
+        '''
+        covariance = np.cov(arr_0, arr_1)[1, 0]
+        return covariance
 
     @staticmethod
-    def pearson_corr_coefficients(
-        arr: np.ndarray, axes: tuple = (0, 1)) -> np.ndarray:
-        pass
+    def pearson_correlation(arr_0: np.ndarray, arr_1: np.ndarray) -> float:
+        '''
+        Calculate two arrays' pearson correlation coefficient
+
+        Args:
+            arr_0: input array 0 \n
+            arr_1: input array 1
+        
+        Return:
+            float: pearson correlation coefficient
+        '''
+        correlation, p = pearsonr(arr_0, arr_1)
+        return correlation
 
 
-class Coherence(AnalyzerBase):
+class Coherence():
     @staticmethod
-    def cross_coherence(arr):
-        pass
+    def scipy_coherence(arr_0: np.ndarray, arr_1: np.ndarray) -> tuple:
+        f, Cxy = coherence(arr_0, arr_1, fs=1000, nperseg=64, noverlap=32)
+        return f, Cxy
+
+    @staticmethod
+    def cross_coherence(arr_0: np.ndarray,
+                        arr_1: np.ndarray,
+                        fs: float = 1,
+                        window_name: str = 'hanning',
+                        nperseg: int = 128,
+                        noverlap: int = None) -> tuple:
+        '''
+        Calculate two arrays' coherence
+
+        Args:
+            arr_0: Input array 0
+            arr_1: Input array 1
+            fs: Sampling frequency
+            window_name: name of window function, default: "hanning"
+            nperseg: length of each segment
+            noverlap: length of overlap
+
+        Return:
+            tuple: frequency array, coherence array
+        '''
+
+        noverlap = noverlap if noverlap else int(nperseg / 2)
+
+        psd_0 = np.zeros(nperseg // 2 + 1)
+        psd_1 = np.zeros(nperseg // 2 + 1)
+        csd = np.zeros(nperseg // 2 + 1, dtype=complex)
+
+        window = getattr(np, window_name)(nperseg)
+        n_segments = (len(arr_0) - noverlap) // (nperseg - noverlap)
+
+        for i in range(n_segments):
+            p_start = i * (nperseg - noverlap)
+            p_end = p_start + nperseg
+            segment_0 = detrend(AnalyzerBase.zero_mean(arr_0[p_start:p_end]))
+            segment_1 = detrend(AnalyzerBase.zero_mean(arr_1[p_start:p_end]))
+            segment_0 = segment_0 * window
+            segment_1 = segment_1 * window
+
+            freq_0 = np.fft.rfft(segment_0)
+            freq_1 = np.fft.rfft(segment_1)
+            psd_0 += np.real(freq_0 * np.conj(freq_0))
+            psd_1 += np.real(freq_1 * np.conj(freq_1))
+            csd += freq_0 * np.conj(freq_1)
+
+        psd_0 = psd_0 / n_segments
+        psd_1 = psd_1 / n_segments
+        csd = csd / n_segments
+
+        coherence = np.abs(csd)**2 / (psd_0 * psd_1)
+        f = np.fft.rfftfreq(nperseg, 1 / fs)
+
+        return f, coherence
